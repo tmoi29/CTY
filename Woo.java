7d4d9ae7be25b5201245b5e66886eea11794e3fc
Chester Huang <chuang9@stuy.edu>
2017-01-11 11:50:24 -0500

    added some methods

--- Woo.java
+++ Woo.java
@@ -1,8 +1,42 @@
 public class Woo
 {
-    private countMoves;
+    private int countMoves;
+
+
+    public Woo() {
+	countMoves = 15; // decide on what number here
+    }
+
+    public static void printWelcome()
+    {
+	System.out.println("lorem ipsum");
+    }
+
+    public static Player createNewPlayer()
+    {
+	// use Keyboard to get user inputs
+	// use Player constructor
+    }
+
+    public static void printStartInfo()
+    {
+
+    }
+
     public static void main(String[] args)
     {
+	Woo newGame = Woo();
+	printWelcome();
+	Player newPlayer = createNewPlayer();
+	Circus newCircus = Circus();
+	Player.printStartInfo();
+	Circus.printStartInfo();
+	Woo.printStartInfo();
+
+	
+
+	
+
 
     }
 
