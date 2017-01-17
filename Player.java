f9d0177d238cd91137096e708dd41a883bf4dfcb
yedohkang913 <ykang@stuy.edu>
2017-01-17 17:04:28 -0500

    updated Woo so that user input errors are handled, and added getName method in Player

--- Player.java
+++ Player.java
@@ -2,97 +2,102 @@ import java.util.ArrayList;
 
 public class Player
 {
-    private String name;
-    private int currentFunds;
-    private int amtFame;
-    private ArrayList<Animal> ownedAnimals;
-    private ArrayList<Integer> trickVal;
-    private ArrayList<Integer> appealVal;
-    //private int price;
+	private String name;
+	private int currentFunds;
+	private int amtFame;
+	private ArrayList<Animal> ownedAnimals;
+	private ArrayList<Integer> trickVal;
+	private ArrayList<Integer> appealVal;
+	//private int price;
 
-    public Player(String n)
-    {
-        ownedAnimals = new ArrayList<Animal>();
-        trickVal = new ArrayList<Integer>();
-        appealVal = new ArrayList<Integer>();
-	currentFunds = 400;
-	amtFame = 0;
-	name = n;
-    }
+	public Player(String n)
+	{
+		ownedAnimals = new ArrayList<Animal>();
+		trickVal = new ArrayList<Integer>();
+		appealVal = new ArrayList<Integer>();
+		currentFunds = 400;
+		amtFame = 0;
+		name = n;
+	}
 
-    public String toString()
-    {
-        String retStr = "";
-        retStr += "Your current funds: $" + currentFunds + "\n";
-        retStr += "Your fame: " + amtFame;
-        return retStr;
-    }
+	public String toString()
+	{
+		String retStr = "";
+		retStr += "Your current funds: $" + currentFunds + "\n";
+		retStr += "Your fame: " + amtFame;
+		return retStr;
+	}
     
-    public String newBalance(Circus c){
-	currentFunds += c.getAmtEarned();
-	amtFame += c.getFameGained();
-	String ret = "Updated Funds: " + currentFunds;
-	ret += "\nUpdated Fame: " + amtFame;
-	return ret;
-    }
-    public void buyAnimal(Animal someAnimal)
-    {
-	if (currentFunds <= 0) {
-	    System.out.println("You do not have enough money to complete the transaction.");
+	public String newBalance(Circus c){
+		currentFunds += c.getAmtEarned();
+		amtFame += c.getFameGained();
+		String ret = "Updated Funds: " + currentFunds;
+		ret += "\nUpdated Fame: " + amtFame;
+		return ret;
 	}
-	else {
-	    ownedAnimals.add(someAnimal);
-	    currentFunds -= someAnimal.getPrice();
-	    System.out.println("You just bought a " + someAnimal.getName() + "!");
-	    System.out.println("You currently have $" + currentFunds + " left.");
-            trickVal.add(someAnimal.getTrickVal());
-            appealVal.add(someAnimal.getAppealVal());
+	
+	public void buyAnimal(Animal someAnimal)
+	{
+		if (currentFunds <= 0) {
+			System.out.println("You do not have enough money to complete the transaction.");
+		}
+		else {
+			ownedAnimals.add(someAnimal);
+			currentFunds -= someAnimal.getPrice();
+			System.out.println("You just bought a " + someAnimal.getName() + "!");
+			System.out.println("You currently have $" + currentFunds + " left.");
+			trickVal.add(someAnimal.getTrickVal());
+			appealVal.add(someAnimal.getAppealVal());
+		}
 	}
-    }
 
-    public String listOwnedAnimals()
-    {
-        String retStr = "";
+	public String listOwnedAnimals()
+	{
+		String retStr = "";
         
-        for (int x = 0; x < ownedAnimals.size(); x += 1)
-            {
-                if (ownedAnimals.get(x).isTrained) {
-                    retStr += "\t" + (x + 1) + ") " + ownedAnimals.get(x).name + "\t#ALREADY TRAINED#" + "\n";
+		for (int x = 0; x < ownedAnimals.size(); x += 1)
+		{
+			if (ownedAnimals.get(x).isTrained) {
+				retStr += "\t" + (x + 1) + ") " + ownedAnimals.get(x).name + "\t#ALREADY TRAINED#" + "\n";
 
-                }
-                else {
-                    retStr += "\t" + (x + 1) + ") " + ownedAnimals.get(x).name + "\n";
+			}
+			else {
+				retStr += "\t" + (x + 1) + ") " + ownedAnimals.get(x).name + "\n";
                    
-                }
+			}
 
-            }
-        return retStr;
-    }
+		}
+		return retStr;
+	}
 
 
-    public int trainAnimal(Animal someAnimal)
-    {
-        if (someAnimal.getIsTrained())
-        {
-            System.out.println("This animal is already trained.");
-            return 0;
-        }
-        else
-        {
-            someAnimal.train();
-            return 1;
-        }
-    }
-    public ArrayList<Integer> getTrickVal() {
-	return trickVal;
-    }
-    public ArrayList<Integer> getAppealVal() {
-	return appealVal;
-    }
+	public int trainAnimal(Animal someAnimal)
+	{
+		if (someAnimal.getIsTrained())
+		{
+			System.out.println("This animal is already trained.");
+			return 0;
+		}
+		else
+		{
+			someAnimal.train();
+			return 1;
+		}
+	}
+	public ArrayList<Integer> getTrickVal() {
+		return trickVal;
+	}
+	public ArrayList<Integer> getAppealVal() {
+		return appealVal;
+	}
 
-    public ArrayList<Animal> getOwnedAnimals()
-    {
-        return ownedAnimals;
-    }
+	public ArrayList<Animal> getOwnedAnimals()
+	{
+		return ownedAnimals;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
 }
